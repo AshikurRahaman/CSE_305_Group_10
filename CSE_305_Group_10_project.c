b38ba71cd606de8eb13f04269a5f4354177167b6
AshikurRahaman <36997873+AshikurRahaman@users.noreply.github.com>
2018-12-14 23:06:40 +0600

    Add files via upload
    first attemt

---
+++ CSE_305_Group_10_project.c
@@ -0,0 +1,24 @@
+///program_skeleton
+#include<stdio.h>
+
+///int Inversion_pair(parameters)
+   ///find the no of inversion pairs of an element
+
+
+///int Find_key(parameters)
+   ///selects the element with most inversion pairs
+
+///void New_array(parameters)
+   ///the selected and inversion paired elements will be eliminated
+   ///the process will continue
+   ///until no element left in the array
+int main()
+{
+    ///scanf(test_case);
+    do{
+        ///scan no_of_objects
+        ///scan the elements
+        ///new_array_function
+
+    }while(!test_case);
+}
